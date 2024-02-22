dba669fcb58d141a2346683533a1482b3b0a9b56
aldinash <aldinashss@gmail.com>
2024-02-22 13:38:06 -0500

    Commit

--- src/main/java/com/aldinash/mydrive/controller/HomeController.java
+++ src/main/java/com/aldinash/mydrive/controller/HomeController.java
@@ -1,8 +1,10 @@
 package com.aldinash.mydrive.controller;
 
 
+import com.aldinash.mydrive.service.FilesService;
 import com.aldinash.mydrive.service.NoteService;
 import com.aldinash.mydrive.service.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,10 +16,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     private final NoteService noteService;
     private final UserService userService;
+    private final FilesService filesService;
 
-    public HomeController(NoteService noteService, UserService userService) {
+    public HomeController(NoteService noteService, UserService userService, FilesService filesService) {
         this.noteService = noteService;
         this.userService = userService;
+        this.filesService = filesService;
     }
 
     // resolve gandezh
@@ -25,6 +29,8 @@ public class HomeController {
     public String home(Authentication authentication, Model model) throws Exception {
         String username = authentication.getName();
         model.addAttribute("notes", noteService.getAllNotes(userService.
+                getUser(username).getUserId()));
+        model.addAttribute("files", filesService.getAllFiles(userService.
                 getUser(username).getUserId()));
         return "home";
     }
